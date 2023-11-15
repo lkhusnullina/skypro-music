eb13b279674e440b7edb9cf73ddcd40c4c564da9
Любовь Хуснуллина <lyuba140296@gmail.com>
2023-11-15 20:02:34 +0500

    skeleton track, audioplayer

--- my-app/src/components/menu/NavMenu.jsx
+++ my-app/src/components/menu/NavMenu.jsx
@@ -1,54 +1,24 @@
-import { useState } from 'react'
-import './NavMenu.css'
-
-function NavMenu() {
-  const [isOpen, setOpen] = useState(false)
-
-  const toggleVisibility = () => {
-    setOpen(!isOpen)
-  }
-
-  return (
-    <nav className="main__nav nav">
-      <div className="nav__logo logo">
-        <img className="logo__image" src="img/logo.png" alt="logo" />
-      </div>
-      <div className="nav__burger burger" onClick={toggleVisibility}>
-        <span className="burger__line" />
-        <span className="burger__line" />
-        <span className="burger__line" />
-      </div>
-      <div className={isOpen ? 'nav__menu menu' : 'nav__menu__hidden menu'}>
-        <ul className="menu__list">
-          <li className="menu__item">
-            <a className="menu__link" xlinkHref="#">
-              Главное
-            </a>
-          </li>
-          <li className="menu__item">
-            <a xlinkHref="#" className="menu__link">
-              Мой плейлист
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="../signin.html" className="menu__link">
-              Войти
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  )
-}
-export default NavMenu
-
 // import { useState } from 'react'
 // import './NavMenu.css'
 
-// function OpenMenu({ isOpen }) {
-//   if (isOpen) {
-//     return (
-//       <div className="nav__menu menu">
+// function NavMenu() {
+//   const [isOpen, setOpen] = useState(false)
+
+//   const toggleVisibility = () => {
+//     setOpen(!isOpen)
+//   }
+
+//   return (
+//     <nav className="main__nav nav">
+//       <div className="nav__logo logo">
+//         <img className="logo__image" src="img/logo.png" alt="logo" />
+//       </div>
+//       <div className="nav__burger burger" onClick={toggleVisibility}>
+//         <span className="burger__line" />
+//         <span className="burger__line" />
+//         <span className="burger__line" />
+//       </div>
+//       <div className={isOpen ? 'nav__menu menu' : 'nav__menu__hidden menu'}>
 //         <ul className="menu__list">
 //           <li className="menu__item">
 //             <a className="menu__link" xlinkHref="#">
@@ -67,30 +37,60 @@ export default NavMenu
 //           </li>
 //         </ul>
 //       </div>
-//     )
-//   }
-//   return ''
-// }
-
-// function NavMenu() {
-//   const [open, setOpen] = useState(false)
-
-//   const toggleVisibility = () => {
-//     setOpen(!open)
-//   }
-
-//   return (
-//     <nav className="main__nav nav">
-//       <div className="nav__logo logo">
-//         <img className="logo__image" src="img/logo.png" alt="logo" />
-//       </div>
-//       <div className="nav__burger burger" onClick={toggleVisibility}>
-//         <span className="burger__line" />
-//         <span className="burger__line" />
-//         <span className="burger__line" />
-//       </div>
-//       <OpenMenu isOpen={open} />
 //     </nav>
 //   )
 // }
 // export default NavMenu
+
+import { useState } from 'react'
+import './NavMenu.css'
+
+function OpenMenu({ isOpen }) {
+  if (isOpen) {
+    return (
+      <div className="nav__menu menu">
+        <ul className="menu__list">
+          <li className="menu__item">
+            <a className="menu__link" xlinkHref="#">
+              Главное
+            </a>
+          </li>
+          <li className="menu__item">
+            <a xlinkHref="#" className="menu__link">
+              Мой плейлист
+            </a>
+          </li>
+          <li className="menu__item">
+            <a href="../signin.html" className="menu__link">
+              Войти
+            </a>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+  return ''
+}
+
+function NavMenu() {
+  const [open, setOpen] = useState(false)
+
+  const toggleVisibility = () => {
+    setOpen(!open)
+  }
+
+  return (
+    <nav className="main__nav nav">
+      <div className="nav__logo logo">
+        <img className="logo__image" src="img/logo.png" alt="logo" />
+      </div>
+      <div className="nav__burger burger" onClick={toggleVisibility}>
+        <span className="burger__line" />
+        <span className="burger__line" />
+        <span className="burger__line" />
+      </div>
+      <OpenMenu isOpen={open} />
+    </nav>
+  )
+}
+export default NavMenu
--- my-app/src/components/track/Track.jsx
+++ my-app/src/components/track/Track.jsx
@@ -1,6 +1,18 @@
 import './Track.css'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 function Track(props) {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 5000)
+  }, [])
+
   return (
     <div className="playlist__item" key={props.track.id}>
       <div className="playlist__track track">
@@ -11,29 +23,50 @@ function Track(props) {
             </svg>
           </div>
           <div className="track__title-text">
-            <a className="track__title-link" href={props.track.trackNamelink}>
-              {props.track.trackName}
-              <span className="track__title-span">
-                {props.track.trackTitlespan}
-              </span>
-            </a>
+            <div>
+              {isLoading ? (
+                <Skeleton />
+              ) : (
+                <a
+                  className="track__title-link"
+                  href={props.track.trackNamelink}
+                >
+                  {props.track.trackName}
+                  <span className="track__title-span">
+                    {props.track.trackTitlespan}
+                  </span>
+                </a>
+              )}
+            </div>
           </div>
         </div>
         <div className="track__author">
-          <a className="track__author-link" href={props.track.authorLink}>
-            {props.track.author}
-          </a>
+          {isLoading ? (
+            <Skeleton />
+          ) : (
+            <a className="track__author-link" href={props.track.authorLink}>
+              {props.track.author}
+            </a>
+          )}
         </div>
         <div className="track__album">
-          <a className="track__album-link" href={props.track.albumLink}>
-            {props.track.album}
-          </a>
+          {isLoading ? (
+            <Skeleton />
+          ) : (
+            <a className="track__album-link" href={props.track.albumLink}>
+              {props.track.album}
+            </a>
+          )}
         </div>
         <div className="track__time">
           <svg className="track__time-svg" alt="time">
             <use xlinkHref="img/icon/sprite.svg#icon-like" />
           </svg>
-          <span className="track__time-text">{props.track.time}</span>
+          {isLoading ? (
+            <span className="track__time-text">0:00</span>
+          ) : (
+            <span className="track__time-text">{props.track.time}</span>
+          )}
         </div>
       </div>
     </div>
