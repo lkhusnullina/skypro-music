6bcf6a58047f72c27a5149a5a6ca4c423827d14f
Любовь Хуснуллина <lyuba140296@gmail.com>
2023-11-22 20:16:31 +0500

    filter

--- my-app/src/components/filter/List.jsx
+++ my-app/src/components/filter/List.jsx
@@ -1,10 +1,16 @@
-// function List({ items }) {
-//   const list = items.map((item) => (
-//     <li className="popup-item" key={item.id}>
-//       ${item.name}
-//     </li>
-//   ))
-//   return <ul>{list}</ul>
-// }
+function List({ items }) {
+  const list = items.map((item) => (
+    <li className="popup-item" key={item.id}>
+      {item.name}
+    </li>
+  ))
+  return (
+    <div className="popupBlock">
+      <div className="popupContainer">
+        <ul className="popup">{list}</ul>
+      </div>
+    </div>
+  )
+}
 
-// export default List
+export default List
--- my-app/src/components/filter/YearButton.jsx
+++ my-app/src/components/filter/YearButton.jsx
@@ -1,7 +1,7 @@
-function YearButton() {
-  return (
-    <div className="filter__button button-year _btn-text">году выпуска</div>
-  )
-}
+// function YearButton() {
+//   return (
+//     <div className="filter__button button-year _btn-text">году выпуска</div>
+//   )
+// }
 
-export default YearButton
+// export default YearButton
