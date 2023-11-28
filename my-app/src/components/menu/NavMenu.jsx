f980f7dc0ade3c57543d00c5e7062ecb50831d84
Любовь Хуснуллина <lyuba140296@gmail.com>
2023-11-28 19:50:36 +0500

    style

--- my-app/src/components/menu/NavMenu.jsx
+++ my-app/src/components/menu/NavMenu.jsx
@@ -1,47 +1,3 @@
-// import { useState } from 'react'
-// import './NavMenu.css'
-
-// function NavMenu() {
-//   const [isOpen, setOpen] = useState(false)
-
-//   const toggleVisibility = () => {
-//     setOpen(!isOpen)
-//   }
-
-//   return (
-//     <nav className="main__nav nav">
-//       <div className="nav__logo logo">
-//         <img className="logo__image" src="img/logo.png" alt="logo" />
-//       </div>
-//       <div className="nav__burger burger" onClick={toggleVisibility}>
-//         <span className="burger__line" />
-//         <span className="burger__line" />
-//         <span className="burger__line" />
-//       </div>
-//       <div className={isOpen ? 'nav__menu menu' : 'nav__menu__hidden menu'}>
-//         <ul className="menu__list">
-//           <li className="menu__item">
-//             <a className="menu__link" xlinkHref="#">
-//               Главное
-//             </a>
-//           </li>
-//           <li className="menu__item">
-//             <a xlinkHref="#" className="menu__link">
-//               Мой плейлист
-//             </a>
-//           </li>
-//           <li className="menu__item">
-//             <a href="../signin.html" className="menu__link">
-//               Войти
-//             </a>
-//           </li>
-//         </ul>
-//       </div>
-//     </nav>
-//   )
-// }
-// export default NavMenu
-
 import { useState } from 'react'
 import './NavMenu.css'
 
@@ -94,3 +50,47 @@ function NavMenu() {
   )
 }
 export default NavMenu
+
+// import { useState } from 'react'
+// import './NavMenu.css'
+
+// function NavMenu() {
+//   const [isOpen, setOpen] = useState(false)
+
+//   const toggleVisibility = () => {
+//     setOpen(!isOpen)
+//   }
+
+//   return (
+//     <nav className="main__nav nav">
+//       <div className="nav__logo logo">
+//         <img className="logo__image" src="img/logo.png" alt="logo" />
+//       </div>
+//       <div className="nav__burger burger" onClick={toggleVisibility}>
+//         <span className="burger__line" />
+//         <span className="burger__line" />
+//         <span className="burger__line" />
+//       </div>
+//       <div className={isOpen ? 'nav__menu menu' : 'nav__menu__hidden menu'}>
+//         <ul className="menu__list">
+//           <li className="menu__item">
+//             <a className="menu__link" xlinkHref="#">
+//               Главное
+//             </a>
+//           </li>
+//           <li className="menu__item">
+//             <a xlinkHref="#" className="menu__link">
+//               Мой плейлист
+//             </a>
+//           </li>
+//           <li className="menu__item">
+//             <a href="../signin.html" className="menu__link">
+//               Войти
+//             </a>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   )
+// }
+// export default NavMenu
