c7c51f8aa50f03c80b59ed0bf1c268fd558eb179
Любовь Хуснуллина <lyuba140296@gmail.com>
2023-11-29 21:05:43 +0500

    tracklist styles

--- my-app/src/components/tracklist/Tracklist.jsx
+++ my-app/src/components/tracklist/Tracklist.jsx
@@ -1,6 +1,7 @@
 import FilterBlock from '../filter/FilterBlock'
 import Track from '../track/Track'
 import './Tracklist.css'
+import styled from 'styled-components'
 
 const tracks = [
   {
@@ -92,40 +93,173 @@ const tracks = [
   },
 ]
 
+const StyledMainCenterblock = styled.div`
+  width: auto;
+  -webkit-box-flex: 3;
+  -ms-flex-positive: 3;
+  flex-grow: 3;
+  padding: 20px 40px 20px 111px;
+`
+const StyledCenterblockSearch = styled.div`
+  width: 100%;
+  border-bottom: 1px solid #4e4e4e;
+  margin-bottom: 51px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+`
+const StyledCenterblockH = styled.h2`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 64px;
+  line-height: 72px;
+  letter-spacing: -0.8px;
+  margin-bottom: 45px;
+`
+const StyledCenterblockContent = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+`
+const StyledSearchSvg = styled.svg`
+  width: 17px;
+  height: 17px;
+  margin-right: 5px;
+  stroke: #ffffff;
+  fill: transparent;
+`
+const StyledSearchText = styled.input`
+  -webkit-box-flex: 100;
+  -ms-flex-positive: 100;
+  flex-grow: 100;
+  background-color: transparent;
+  border: none;
+  padding: 13px 10px 14px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
+  $::-webkit-input-placeholder {
+    background-color: transparent;
+    color: #ffffff;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+  }
+
+  $:-ms-input-placeholder {
+    background-color: transparent;
+    color: #ffffff;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+  }
+
+  $::-ms-input-placeholder {
+    background-color: transparent;
+    color: #ffffff;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+  }
+  $::placeholder {
+    background-color: transparent;
+    color: #ffffff;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+  }
+`
+const StyledContentTitle = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: justify;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  margin-bottom: 24px;
+`
+const StyledContentPlaylist = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  overflow-y: auto;
+`
+const StyledPlaylistTitleCol = styled.div`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 24px;
+  letter-spacing: 2px;
+  color: #696969;
+  text-transform: uppercase;
+`
+const StyledPlaylistTitleSvg = styled.svg`
+  width: 12px;
+  height: 12px;
+  fill: transparent;
+  stroke: #696969;
+`
+// const Styled = styled.div``
+// const Styled = styled.div``
+// const Styled = styled.div``
+
 function Tracklist() {
   return (
-    <div className="main__centerblock centerblock">
-      <div className="centerblock__search search">
-        <svg className="search__svg">
+    <StyledMainCenterblock>
+      <StyledCenterblockSearch>
+        <StyledSearchSvg>
           <use xlinkHref="img/icon/sprite.svg#icon-search" />
-        </svg>
-        <input
-          className="search__text"
-          type="search"
-          placeholder="Поиск"
-          name="search"
-        />
-      </div>
-      <h2 className="centerblock__h2">Треки</h2>
+        </StyledSearchSvg>
+        <StyledSearchText type="search" placeholder="Поиск" name="search" />
+      </StyledCenterblockSearch>
+      <StyledCenterblockH>Треки</StyledCenterblockH>
       <FilterBlock />
-      <div className="centerblock__content">
-        <div className="content__title playlist-title">
+      <StyledCenterblockContent>
+        <StyledContentTitle>
           <div className="playlist-title__col col01">Трек</div>
           <div className="playlist-title__col col02">ИСПОЛНИТЕЛЬ</div>
           <div className="playlist-title__col col03">АЛЬБОМ</div>
           <div className="playlist-title__col col04">
-            <svg className="playlist-title__svg" alt="time">
+            <StyledPlaylistTitleSvg alt="time">
               <use xlinkHref="img/icon/sprite.svg#icon-watch" />
-            </svg>
+            </StyledPlaylistTitleSvg>
           </div>
-        </div>
-        <div className="content__playlist playlist">
+        </StyledContentTitle>
+        <StyledContentPlaylist>
           {tracks.map((t) => (
             <Track key={t.id} track={t} />
           ))}
-        </div>
-      </div>
-    </div>
+        </StyledContentPlaylist>
+      </StyledCenterblockContent>
+    </StyledMainCenterblock>
   )
 }
 
