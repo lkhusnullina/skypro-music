3414b1cce49b6c2d0874e1b497b9332245759ffa
Любовь Хуснуллина <lyuba140296@gmail.com>
2024-01-01 16:15:25 +0500

    registration

--- my-app/src/api.js
+++ my-app/src/api.js
@@ -1,3 +1,4 @@
+const logHost = 'https://skypro-music-api.skyeng.tech/user';
 
 export async function getTrackAll() {
    const response =  await fetch('https://skypro-music-api.skyeng.tech/catalog/track/all/', {
@@ -13,3 +14,32 @@ export async function getTrackAll() {
    return data;    
 } 
 
+export  async function registerUser({ email, password, username }) {
+   const response = await fetch(logHost + '/signup/', {
+     method: 'POST',
+     body: JSON.stringify({
+      email,
+      password,
+      username,
+     }),
+     headers: {
+       "content-type": "application/json",
+     },
+   })
+   return response.json();
+};
+ 
+export async function loginUser({ email, password }) {
+   const response = await fetch(logHost + '/login/', {
+     method: 'POST',
+     body: JSON.stringify({
+       email,
+       password,
+     }),
+     headers: {
+      "content-type": "application/json",
+     },
+   })
+   return response.json();
+};
+ 
--- my-app/src/pages/authpage/AuthPage.jsx
+++ my-app/src/pages/authpage/AuthPage.jsx
@@ -1,24 +1,87 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import * as S from "./AuthPage.styles";
 import { useEffect, useState } from "react";
 import { useUserContext } from "../../context/user";
+import { loginUser, registerUser } from "../../api";
 
 export default function AuthPage({ isLoginMode = false }) {
-  const [error, setError] = useState(null);
   const {login} = useUserContext();
+  const [error, setError] = useState(null);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
+  const [username, setUsername] = useState("");
+//   const navigate = useNavigate();
 
   const handleLogin = async ({ email, password }) => {
+    if (!email) {
+        setError("Укажите почту");
+        return;
+      }
+  
+      if (!password) {
+        setError("Укажите пароль");
+        return;
+      }
+
+    loginUser({
+        email: email,
+        password: password,
+    }).then((user) => {
+        isLoginMode(true)
+        setEmail(user.email)
+    }).catch((error) => {
+        console.warn(error);
+        alert(`Выполняется вход: ${email} ${password}`);
+        setError("Неизвестная ошибка входа");
+    })
+    // api login
+    // api.login(fcghfghfhg).then().catch(=>setError)
+
     login(email)
-    alert(`Выполняется вход: ${email} ${password}`);
-    setError("Неизвестная ошибка входа");
+    
   };
 
-  const handleRegister = async () => {
-    alert(`Выполняется регистрация: ${email} ${password}`);
-    setError("Неизвестная ошибка регистрации");
+  const handleRegister = async ({ email, password, username }) => {
+    setError("");
+    
+    if (!email) {
+        setError("Укажите почту");
+        return;
+    }
+
+    if (!password) {
+        setError("Укажите пароль");
+        return;
+    }
+
+    if (!repeatPassword) {
+        setError("Укажите повторный пароль");
+        return;
+    }
+
+    if (password !== repeatPassword) {
+        setError("Пароли не совпадают");
+        return;
+    }
+    
+    registerUser({
+        email: email, 
+        password: password, 
+        username: username,
+    })
+    .then((user) => {
+        console.log(111);
+        console.log(user);
+        isLoginMode(true)
+        setEmail(user.email)
+    })
+    .catch((error) => {
+        console.warn(error);
+        // alert(`Выполняется регистрация: ${email} ${password}`);
+        setError("Неизвестная ошибка регистрации");
+    })
+    
   };
 
   // Сбрасываем ошибку если пользователь меняет данные на форме или меняется режим формы
@@ -61,7 +124,7 @@ export default function AuthPage({ isLoginMode = false }) {
               <S.PrimaryButton onClick={() => handleLogin({ email, password })}>
                 Войти
               </S.PrimaryButton>
-              <Link to="/register">
+              <Link to="/registration">
                 <S.SecondaryButton>Зарегистрироваться</S.SecondaryButton>
               </Link>
             </S.Buttons>
@@ -69,6 +132,15 @@ export default function AuthPage({ isLoginMode = false }) {
         ) : (
           <>
             <S.Inputs>
+            <S.ModalInput
+                type="text"
+                name="username"
+                placeholder="Имя пользователя"
+                value={username}
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                }}
+              />
               <S.ModalInput
                 type="text"
                 name="login"
@@ -99,7 +171,7 @@ export default function AuthPage({ isLoginMode = false }) {
             </S.Inputs>
             {error && <S.Error>{error}</S.Error>}
             <S.Buttons>
-              <S.PrimaryButton onClick={handleRegister}>
+              <S.PrimaryButton onClick={() => handleRegister({ email, password, username })}>
                 Зарегистрироваться
               </S.PrimaryButton>
             </S.Buttons>
